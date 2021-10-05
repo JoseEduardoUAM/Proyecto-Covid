bd9e82d1eca2590220c81cffafde8ef5bbff5917
JoseEduardoUAM <al2172002687@azc.uam.mx>
2021-10-04 20:22:29 -0500

    Archivos JS para la logica y comunicacion con el servidor

---
+++ Public/Familiar/script.js/identificadores.js
@@ -0,0 +1,17 @@
+export let form = document.getElementById('Formulario');
+    export let pantalla1 = document.getElementById('PrimeraPantalla');
+        export let inputs = [
+            document.getElementById('nombrePaciente'),
+            document.getElementById('apellidoPaciente'),
+            document.getElementById('numeroExpediente'),
+            document.getElementById('nombreFamiliar'),
+            document.getElementById('apellidoFamiliar')
+        ];
+        export let conBtn = document.getElementById('ContenedorBoton');
+            export let btnURL = document.getElementById('BotonURL');
+        export let conAviso = document.getElementById('ContenedorAviso');
+        export let btnEnviar = document.getElementById('BotonEnviar');
+    export let pantalla2 = document.getElementById('SegundaPantalla');
+        export let btnIniciar = document.getElementById('BotonIniciar');
+
+export let URL = document.getElementById('URL');
---
+++ Public/Familiar/script.js/main.js
@@ -0,0 +1,25 @@
+import { btnEnviar, btnURL, conAviso, conBtn, form, URL } from './identificadores.js';
+import { CrearDatos, VerificarDatos } from './funciones.js';
+
+let urlSala;
+
+const socket = io('/');
+
+socket.on( 'recibirURL' , (url) => {
+	urlSala = url;
+    URL.value = url;
+    form.action = url;
+});
+
+btnURL.onclick = () =>{
+    socket.emit( 'solicitarURL' ,  );
+    conBtn.style.display = "none";
+    conAviso.style.display = null;
+    btnEnviar.disabled = null;
+}
+
+btnEnviar.onclick = () =>{
+    if( VerificarDatos() ){
+        socket.emit( 'nuevaSala' , CrearDatos(urlSala) );
+    }
+}
